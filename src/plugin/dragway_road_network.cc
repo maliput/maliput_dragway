c4871d1e13433befe5e9f056d96a5039bf92721c
Franco Cipollone <franco.c@ekumenlabs.com>
2020-12-21 16:03:51 -0300

    Modify dragway plugin to build a road network from a parameter list.

--- src/plugin/dragway_road_network.cc
+++ src/plugin/dragway_road_network.cc
@@ -6,11 +6,40 @@
 namespace maliput {
 namespace dragway {
 namespace plugin {
+namespace {
+
+maliput::dragway::BuildProperties GetPropertiesFromStringMap(const std::map<std::string, std::string> parameters) {
+  BuildProperties properties{};
+  auto it = parameters.find("num_lanes");
+  if (it != parameters.end()) {
+    properties.num_lanes = std::stoi(it->second);
+  }
+  it = parameters.find("length");
+  if (it != parameters.end()) {
+    properties.length = std::stod(it->second);
+  }
+  it = parameters.find("lane_width");
+  if (it != parameters.end()) {
+    properties.lane_width = std::stod(it->second);
+  }
+  it = parameters.find("shoulder_width");
+  if (it != parameters.end()) {
+    properties.shoulder_width = std::stod(it->second);
+  }
+  it = parameters.find("maximum_height");
+  if (it != parameters.end()) {
+    properties.maximum_height = std::stod(it->second);
+  }
+  return properties;
+}
+
+}  // namespace
 
 class DragwayRoadNetwork : public maliput::plugin::RoadNetworkPlugin {
  public:
-  std::unique_ptr<const maliput::api::RoadNetwork> LoadRoadNetwork() const override {
-    return maliput::dragway::BuildRoadNetwork();
+  std::unique_ptr<const maliput::api::RoadNetwork> LoadRoadNetwork(
+      const std::map<std::string, std::string> parameters) const override {
+    return maliput::dragway::BuildRoadNetwork(GetPropertiesFromStringMap(parameters));
   }
 };
 
