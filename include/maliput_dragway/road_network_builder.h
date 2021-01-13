374081d600be73c7317da3365b6446e8f674914d
Franco Cipollone <53065142+francocipollone@users.noreply.github.com>
2021-01-13 15:43:45 -0300

    Implements a maliput RoadNetworLoader plugin (#24)

---
+++ include/maliput_dragway/road_network_builder.h
@@ -0,0 +1,32 @@
+// Copyright 2021 Toyota Research Institute
+#pragma once
+
+#include "maliput/api/road_geometry.h"
+#include "maliput/api/road_network.h"
+
+#include <memory>
+
+namespace maliput {
+namespace dragway {
+
+/// Contains the attributes needed for building a dragway::RoadGeometry.
+struct RoadGeometryConfiguration {
+  /// Number of lanes.
+  int num_lanes{2};
+  /// Length of the lanes.
+  double length{10};
+  /// Width of the lanes.
+  double lane_width{3.7};
+  /// Width of the shoulders of the road.
+  double shoulder_width{3.};
+  /// Maximum height above the road surface.
+  double maximum_height{5.2};
+};
+
+/// Builds an api::RoadNetwork based on Dragway implementation.
+/// @param road_geometry_configuration Holds the properties to build the RoadNetwork.
+/// @return A maliput::api::RoadNetwork.
+std::unique_ptr<const api::RoadNetwork> BuildRoadNetwork(const RoadGeometryConfiguration& road_geometry_configuration);
+
+}  // namespace dragway
+}  // namespace maliput
---
+++ src/plugin/road_network.cc
@@ -0,0 +1,58 @@
+// Copyright 2021 Toyota Research Institute
+#include <memory>
+
+#include "maliput/plugin/road_network_loader.h"
+#include "maliput_dragway/road_network_builder.h"
+
+namespace maliput {
+namespace dragway {
+namespace plugin {
+namespace {
+
+// Return a dragway::RoadGeometryConfiguration object out of a map of strings.
+// When a property is missing it uses the default value from dragway::RoadGeometryConfiguration.
+// @param parameters  A dictionary of properties to fill in a dragway::RoadGeometryConfiguration struct.
+//                    Keys are the names of attributes in dragway::RoadGeometryConfiguration.
+// @returns A dragway::RoadGeometryConfiguration.
+maliput::dragway::RoadGeometryConfiguration GetPropertiesFromStringMap(
+    const std::map<std::string, std::string>& parameters) {
+  RoadGeometryConfiguration rg_configuration{};
+  auto it = parameters.find("num_lanes");
+  if (it != parameters.end()) {
+    rg_configuration.num_lanes = std::stoi(it->second);
+  }
+  it = parameters.find("length");
+  if (it != parameters.end()) {
+    rg_configuration.length = std::stod(it->second);
+  }
+  it = parameters.find("lane_width");
+  if (it != parameters.end()) {
+    rg_configuration.lane_width = std::stod(it->second);
+  }
+  it = parameters.find("shoulder_width");
+  if (it != parameters.end()) {
+    rg_configuration.shoulder_width = std::stod(it->second);
+  }
+  it = parameters.find("maximum_height");
+  if (it != parameters.end()) {
+    rg_configuration.maximum_height = std::stod(it->second);
+  }
+  return rg_configuration;
+}
+
+// Implementation of a maliput::plugin::RoadNetworkLoader using dragway backend.
+class RoadNetworkLoader : public maliput::plugin::RoadNetworkLoader {
+ public:
+  std::unique_ptr<const maliput::api::RoadNetwork> operator()(
+      const std::map<std::string, std::string>& properties) const override {
+    return maliput::dragway::BuildRoadNetwork(GetPropertiesFromStringMap(properties));
+  }
+};
+
+}  // namespace
+
+REGISTER_ROAD_NETWORK_LOADER_PLUGIN("maliput_dragway", RoadNetworkLoader);
+
+}  // namespace plugin
+}  // namespace dragway
+}  // namespace maliput
