9c69258d9f577129f1186ffa5aa0a976a2fd009e
Franco Cipollone <franco.c@ekumenlabs.com>
2021-01-18 11:10:46 -0300

    Fix scan-build bug.

--- test/plugin/road_network_plugin_test.cc
+++ test/plugin/road_network_plugin_test.cc
@@ -24,20 +24,23 @@ GTEST_TEST(RoadNetworkLoader, VerifyRoadNetworkPlugin) {
   plugin::MaliputPluginManager manager{};
   plugin::MaliputPlugin const* rn_plugin{nullptr};
   EXPECT_NO_THROW(rn_plugin = manager.GetPlugin(kDragwayPluginId));
-  EXPECT_NE(nullptr, rn_plugin);
+  ASSERT_NE(nullptr, rn_plugin);
 
   // Check dragway plugin is obtained.
-  EXPECT_EQ(kDragwayPluginId.string(), rn_plugin->GetId());
-  EXPECT_EQ(plugin::MaliputPluginType::kRoadNetworkLoader, rn_plugin->GetType());
   std::unique_ptr<maliput::plugin::RoadNetworkLoader> rn_loader{nullptr};
-  EXPECT_NO_THROW(rn_loader = rn_plugin->ExecuteSymbol<std::unique_ptr<plugin::RoadNetworkLoader>>(
-                      plugin::RoadNetworkLoader::GetEntryPoint()));
-  EXPECT_NE(nullptr, rn_loader);
+  if (rn_plugin != nullptr) {
+    // Without checking rn_plugin nullptrness the scan-build failed in this line.
+    EXPECT_EQ(kDragwayPluginId.string(), rn_plugin->GetId());
+    EXPECT_EQ(plugin::MaliputPluginType::kRoadNetworkLoader, rn_plugin->GetType());
+    EXPECT_NO_THROW(rn_loader = rn_plugin->ExecuteSymbol<std::unique_ptr<plugin::RoadNetworkLoader>>(
+                        plugin::RoadNetworkLoader::GetEntryPoint()));
+  }
+  ASSERT_NE(nullptr, rn_loader);
 
   // Check dragway RoadNetwork is constructible.
   std::unique_ptr<const maliput::api::RoadNetwork> rn{nullptr};
   EXPECT_NO_THROW(rn = (*rn_loader)(rg_dragway_properties));
-  EXPECT_NE(nullptr, rn);
+  ASSERT_NE(nullptr, rn);
   auto dragway_rg = dynamic_cast<const RoadGeometry*>(rn->road_geometry());
   EXPECT_NE(nullptr, dragway_rg);
 }
